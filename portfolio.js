1725efff27c1c31e42623d4e5fd74fb392165bf4
Bertrand008 <12202968@student.pxl.be>
2023-06-17 22:22:54 +0200

    veranderingen

--- portfolio.js
+++ portfolio.js
@@ -16,18 +16,18 @@ photoshopButton.onclick = function () {
 
 illustratorButton.onclick = function () {
     document.getElementById("project_titel").innerHTML = "Illustrator";
-    photo1.innerHTML = '<img src="assets/ai-foto-1.png" class="img-2 col-12 col-md-8 p-md-5 mx-md-5" alt="ps-foto-1">';
-    photo2.innerHTML = '<img src="assets/ai-foto-2.png" class="img-4 col-12 px-0 py-md-4" alt="ps-foto-2">';
-    photo3.innerHTML = '<img src="assets/ai-foto-3.png" class="img-4 px-0 pe-lg-4 p-md-4 me-md-5" alt="ps-foto-4">';
+    photo1.innerHTML = '<img src="assets/ai-foto-1.png" class="img-2 col-12 col-md-8 p-md-5 mx-md-5" alt="ai-foto-1">';
+    photo2.innerHTML = '<img src="assets/ai-foto-2.png" class="img-4 col-12 col-md-7 col-lg-12 pt-md-5 px-0 py-md-4" alt="ai-foto-2">';
+    photo3.innerHTML = '<img src="assets/ai-foto-3.png" class="img-4 px-0 pe-lg-4 p-md-4 me-md-5" alt="ai-foto-3">';
     photo4.innerHTML = '<img src="assets/banner.png" class="img-3 pe-lg-4 p-md-4 me-md-5" alt="ai-foto-4">';
 };
 
 figmaButton.onclick = function () {
     document.getElementById("project_titel").innerHTML = "Figma";
     photo1.innerHTML = '<img src="assets/fg-foto-1.png" class="img-2 col-12 col-md-8 p-md-5 mx-md-5" alt="ps-foto-1">';
-    photo2.innerHTML = '<img src="assets/fg-foto-2.png" class="img-3 pe-lg-4 p-md-4 me-md-5" alt="ps-foto-4">';
-    photo3.innerHTML = '<img src="assets/fg-foto-3.png" class="img-3 pe-lg-4 p-md-4 me-md-5" alt="ps-foto-4">';
-    photo4.innerHTML = '<img src="assets/fg-foto-4.png" class="img-3 pe-lg-4 p-md-4 me-md-5" alt="ai-foto-4">';
+    photo2.innerHTML = '<img src="assets/fg-foto-2.png" class="img-3 pe-lg-4 p-md-4 me-md-5" alt="fg-foto-2">';
+    photo3.innerHTML = '<img src="assets/fg-foto-3.png" class="img-3 pe-lg-4 p-md-4 me-md-5" alt="fg-foto-3">';
+    photo4.innerHTML = '<img src="assets/fg-foto-4.png" class="img-3 pe-lg-4 p-md-4 me-md-5" alt="fg-foto-4">';
 };
 
 
@@ -35,3 +35,16 @@ let btncolor = document.getElementById("project_titel").style.color = "#fff";
 let textSize = document.getElementById("project_titel").style.fontSize = "60px";
 let textFont = document.getElementById("project_titel").style.fontFamily = "roboto-black";
 let textWeight = document.getElementById("project_titel").style.fontWeight = "bold";
+
+var i=0, text;
+
+text= "Tuning"
+
+function typing(){
+    if(i<text.length){
+     document.getElementById("text").innerHTML += text.charAt(i);
+     i++
+     setTimeout(typing,50);   
+    }
+}
+typing();
