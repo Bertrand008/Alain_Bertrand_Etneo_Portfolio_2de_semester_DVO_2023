3165c5704dc4a853e0ac67944ff10a74c8177bf2
Bertrand008 <12202968@student.pxl.be>
2023-06-19 15:19:53 +0200

    veranderingen in index pagina

--- portfolio.js
+++ portfolio.js
@@ -19,7 +19,7 @@ illustratorButton.onclick = function () {
     photo1.innerHTML = '<img src="assets/ai-foto-1.png" class="img-2 col-12 col-md-8 p-md-5 mx-md-5" alt="ai-foto-1">';
     photo2.innerHTML = '<img src="assets/ai-foto-2.png" class="img-4 col-12 col-md-7 col-lg-12 pt-md-5 px-0 py-md-4" alt="ai-foto-2">';
     photo3.innerHTML = '<img src="assets/ai-foto-3.png" class="img-4 px-0 pe-lg-4 p-md-4 me-md-5" alt="ai-foto-3">';
-    photo4.innerHTML = '<img src="assets/banner.png" class="img-3 pe-lg-4 p-md-4 me-md-5" alt="ai-foto-4">';
+    photo4.innerHTML = '<img src="assets/banner.png" class="img-4 col-12 col-md-7 col-lg-12 pt-md-5 px-0 py-md-4" alt="ai-foto-4">';
 };
 
 figmaButton.onclick = function () {
